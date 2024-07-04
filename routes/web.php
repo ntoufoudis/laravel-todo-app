36b8d0d5b9a3d16bdd80c6819d862ee5496ff6fd
Vasileios Ntoufoudis <info@ntoufoudis.com>
2024-07-04 11:09:30 +0300

    Clean Up

--- routes/web.php
+++ routes/web.php
@@ -1,15 +1,9 @@
 <?php
 
-use App\Http\Controllers\HomeController;
 use App\Livewire\Home;
 use Illuminate\Support\Facades\Route;
 
-//Route::get('/', HomeController::class)->name('home');
 Route::get('/', Home::class)->name('home');
-
-//Route::get('/', function () {
-//    return view('dashboard');
-//});
 
 Route::middleware([
     'auth:sanctum',
